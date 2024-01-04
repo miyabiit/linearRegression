82459f7dfa6d12b94a2121fac6b2e09f8839c397
miyabiit <yamada.masahiro@gmail.com>
2024-01-04 22:47:01 +0900

    remove boston_data

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn import preprocessing
-from sklearn.datasets import load_boston
+#from sklearn.datasets import load_boston
 from sklearn.model_selection import train_test_split
 
 st.title("ボストン市の住宅価格の重回帰分析")
