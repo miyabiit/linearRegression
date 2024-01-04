738a4183e0bfbaf6ff367b7eb7d2d87d3204488c
miyabiit <yamada.masahiro@gmail.com>
2024-01-04 22:06:42 +0900

    set library

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,10 @@
 import streamlit as st
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from scikit-learn import preprocessing
+from scikit-learn.datasets import load_boston
+from scikit-learn.model_selection import train_test_split
 
-x = 10
-"x:",x
+st.title("ボストン市の住宅価格の重回帰分析")
