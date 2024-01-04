22bf69789a61bda357496880e4e22c9dc9ee9e91
miyabiit <yamada.masahiro@gmail.com>
2024-01-04 23:27:26 +0900

    set all library & modules

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,9 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn import preprocessing
 #from sklearn.datasets import load_boston
+from sklearn.datasets import fetch_california_housing
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
 
-st.title("ボストン市の住宅価格の重回帰分析")
+st.title("California 住宅価格の重回帰分析")
