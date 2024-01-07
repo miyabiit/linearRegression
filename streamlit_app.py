5f17ac61f573ea2b8d96e491e80b17a44a9eb7b5
miyabiit <yamada.masahiro@gmail.com>
2024-01-07 12:59:57 +0900

    add figure

--- streamlit_app.py
+++ streamlit_app.py
@@ -95,3 +95,71 @@ x_train, x_value, y_train, y_value = train_test_split(
         test_size=test_size,
         random_state=random_seed
         )
+
+# 回帰モデルの作成と訓練
+regressor = LinearRegression()
+regressor.fit(x_train, y_train)
+y_pred_train = regressor.predict(x_train)
+y_pred_value = regressor.predict(x_value)
+
+if "PRICES" in log_features:
+    y_pred_value, y_pred_train = np.exp(y_pred_value), np.exp(y_pred_train)
+    y_value, y_train = np.exp(y_value), np.exp(y_train)
+
+
+"""
+## 結果の表示
+"""
+
+# モデルの精度
+R2 = r2_score(y_value, y_pred_value)
+st.write(f'R2値:{R2:.2f}')
+
+"""
+## 表示
+"""
+left_column, right_column = st.columns([1,2])
+show_train = left_column.radio(
+        "訓練データの結果を表示:",
+        ("Yes","No"))
+show_value = right_column.radio(
+        "検証データの結果を表示:",
+        ('Yes','No'))
+
+# 目的変数の最大値を取得
+y_max_train = max([max(y_train),max(y_pred_train)])
+y_max_value = max([max(y_value),max(y_pred_value)])
+y_max = int(max([y_max_train,y_max_value]))
+
+# 動的に軸の範囲を設定
+left_column, right_column = st.columns([1,2])
+x_min = left_column.number_input(
+        'X軸の最小値',
+        value=0, step=1)
+x_max = right_column.number_input(
+        'X軸の最大値',
+        value=y_max, step=1)
+
+left_column, right_column = st.columns([1,2])
+y_min = left_column.number_input(
+        'Y軸の最小値',
+        value=0, step=1)
+y_max = right_column.number_input(
+        'Y軸の最大値',
+        value=y_max, step=1)
+
+# グラフ
+fig = plt.figure()
+ax = fig.add_subplot(1,1,1)
+if show_train == 'Yes':
+    ax.scatter(y_train, y_pred_train, linewidths=0, color="r", s=2)
+if show_value == 'Yes':
+    ax.scatter(y_value, y_pred_value, linewidths=0, color="b", s=2)
+ax.grid(True)
+ax.set_xlim(x_min, x_max)
+ax.set_ylim(y_min, y_max)
+ax.set_xlabel("PRICES")
+ax.set_ylabel("Prediction of PRICES")
+ax.legend(['TrainingData','ValidationData'])
+st.pyplot(fig) 
+
